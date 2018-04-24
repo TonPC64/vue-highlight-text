8215a69b9208a9a48f071febf0fabedecaf3ffee
Chanwit Piromplad <kingkong2103@gmail.com>
2018-04-24 17:04:40 +0700

    simple but not yet build

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <Highlight :message="msg" keyword="App" />
+  </div>
+</template>
+
+<script>
+import Highlight from './components/VueHighlightText'
+export default {
+  name: 'app',
+  components: {
+    Highlight
+  },
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/VueHighlightText.vue
@@ -0,0 +1,28 @@
+<template>
+  <span v-html="highlightSeach(message, keyword)"></span>
+</template>
+
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true
+    },
+    keyword: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    highlightSeach (message, keyword) {
+      let match = new RegExp(keyword, 'g')
+      if (match.test(message)) {
+        return message.replace(match, `<span style="color: #0084FF;">${this.keyword}</span>`)
+      }
+      return message
+    },
+  }
+}
+</script>
